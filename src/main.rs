ca24f0620f933fef7d9ae5470855d1465f85402f
Memas Deligeorgakis <deligeorgakismemas@googlemail.com>
2023-01-06 13:22:41 +0100

    wip: using `Client` from forked `tendermint-rpc`, not building

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use tendermint_rpc_only_client::Client as TendermintClient;
+
+struct NamadaClient;
+
+impl TendermintClient for NamadaClient {}
+
 fn main() {
+    let _namada_client = NamadaClient;
     println!("Hello, world!");
 }
